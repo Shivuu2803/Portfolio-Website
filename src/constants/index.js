1778b9313dcf758a366df630fcf15cd5200c3551
Shivansh Mishra <94588491+Shivuu2803@users.noreply.github.com>
2024-10-24 02:59:31 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ import project11 from "../assets/projects/project-11.png";
 export const HERO_CONTENT = `I am a full-stack developer specializing in the MERN stack, with expertise in building responsive web and mobile applications. Currently pursuing a degree in Computer Science and Engineering at Lovely Professional University, I've developed projects like a React-based Gemini clone and a cross-platform fitness tracking app using React Native. My skills include JavaScript, ReactJS, React Native, NodeJS, ExpressJS, MongoDB, and UI/UX design with Figma. I’m passionate about creating innovative solutions and continuously honing my skills to excel in the tech industry.`;
 
 export const ABOUT_TEXT1 = `
-As a versatile full-stack developer, I bring a strong foundation in the MERN stack, coupled with extensive hands-on experience in building efficient and user-friendly web and mobile applications. My expertise in React, React Native, Node.js, and MongoDB empowers me to tackle complex challenges and deliver high-quality solutions that meet the needs of both users and stakeholders. My journey in tech is driven by a deep curiosity and an unwavering commitment to innovation.`
+As a versatile full-stack developer, I bring a strong foundation in the MERN stack, coupled with extensive hands-on experience in building efficient and user-friendly web and mobile applications. My expertise in React, React Native, Node.js, and MongoDB empowers me to tackle complex challenges and deliver high-quality solutions that meet the needs of both users and stakeholders. My journey in tech is driven by a deep curiosity and an unwavering commitment to innovation.`;
 
 export const ABOUT_TEXT2 = `
 
@@ -23,79 +23,21 @@ Outside of my professional pursuits, I am passionate about dance, music, and bad
 
 export const PROJECTS = [
   {
-<<<<<<< HEAD
-=======
-    title: "Dice Game",
-    image: project5,
-    description:
-      "Dice Game is a web-based, two-player game where players roll dice to reach a target score. It features alternating turns, score tracking, and a risk element where rolling double six resets the player's score to zero, highlighting essential skills in game logic and interactive web development.",
-    technologies: ["CSS", "React"],
-    github:"https://github.com/Shivuu2803/Dice-Game"
-  },
-  {
->>>>>>> 5efdee5b13296de5591edbda92cb7ac8ea419fd1
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["TailwindCSS", "Javascript", "React"],
-<<<<<<< HEAD
     github: "https://github.com/Shivuu2803/Portfolio-Website",
-    hostedLink: "https://portfolio-website-indol-rho.vercel.app/"
-=======
-    github:"https://github.com/Shivuu2803/Portfolio-Website"
->>>>>>> 5efdee5b13296de5591edbda92cb7ac8ea419fd1
+    hostedLink: "https://portfolio-website-indol-rho.vercel.app/",
   },
   {
     title: "Blog-Website",
     image: project2,
     description:
-<<<<<<< HEAD
       "Developed a full-stack Blog App using the MERN stack, emphasizing user authentication, CRUD operations, state management, and responsive UI design. Integrated MongoDB for data persistence, Express.js and Node.js for server-side logic, and React for building a dynamic frontend interface.",
     technologies: ["ReactJS", "NodeJS", "ExpressJS", "Mongodb", "JWT"],
-    github: "https://github.com/Shivuu2803/Blog-Website"
-=======
-      "Developed Gemini 2.0, a React-based clone of the Gemini protocol with a redesigned user interface, integrating Gemini API forenhanced functionality. Leveraged React components, state management, and responsive design to ensure seamless navigation andinteractive user experience, while integrating real-time data from Gemini API to provide dynamic content. ",
-    technologies: ["CSS", "React"],
-    github:"https://github.com/Shivuu2803/Gemini-2.0"
-  },
-  {
-    title: "Currency Convertor",
-    image: project11,
-    description:
-      "Currency Converter is a web tool for real-time currency conversion, featuring a simple interface and instant results. It highlights skills in API integration and practical financial application design.",
-    technologies: ["HTML", "CSS", "Javascript"],
-    github:"https://github.com/Shivuu2803/Currency-Converter-"
-  },
-
-  {
-    title: "Simon Game",
-    image: project8,
-    description:
-      " fun Simon Game  incorporating HTML audio for engaging user experience,implementing sound sequences, and designing an intuitive interface to deliver an entertaining web app with enhanced gameplayand sound. ",
-    technologies: ["HTML", "CSS", "Javascript", "jQuery"],
-    github:"https://github.com/Shivuu2803/Simon-Game-FrontEnd-"
-  },
-  {
-    title: "Drum Kit",
-    image: project7,
-    description:
-      "Drum Kit is an interactive web-based project that allows users to play different drum sounds by pressing corresponding keys on the keyboard or clicking on-screen buttons. The project showcases the integration of sound effects with user interactions, creating a fun and engaging experience for practicing event handling and multimedia in web development.",
-    technologies: ["HTML", "CSS", "Javascript"],
-    github:"https://github.com/Shivuu2803/Drum-Kit-FrontEnd-"
->>>>>>> 5efdee5b13296de5591edbda92cb7ac8ea419fd1
-  },
-  {
-    title: "Myfit App",
-    image: project9,
-    description:
-      " Developed myFit-App using React Native, focusing on cross-platform fitness tracking, intuitive user interfaces, seamless navigation, and best practices in state management and API integration.",
-    technologies: ["TailwindCSS", "React-Native"],
-<<<<<<< HEAD
-    github: "https://github.com/Shivuu2803/myFit-App"
-=======
-    github:"https://github.com/Shivuu2803/myFit-App"
->>>>>>> 5efdee5b13296de5591edbda92cb7ac8ea419fd1
+    github: "https://github.com/Shivuu2803/Blog-Website",
   },
   {
     title: "Product API",
@@ -104,25 +46,7 @@ export const PROJECTS = [
       "Developed a product API using Node.js, enabling CRUD operations for managing product data. Focused on building RESTful endpoints, efficient data handling, and backend logic. Followed best practices in API design to ensure scalability and seamless integration with frontend applications.",
     technologies: ["NodeJS", "ExpressJS", "Mongodb", "REST API", "Postman"],
     github: "https://github.com/Shivuu2803/ProductApi",
-    hostedLink: "https://productapi-production-af22.up.railway.app/"
-  },
-  {
-    title: "Gemini 2.0",
-    image: project6,
-    description:
-      "Developed Gemini 2.0, a React-based clone of the Gemini protocol with a redesigned user interface, integrating Gemini API forenhanced functionality. Leveraged React components, state management, and responsive design to ensure seamless navigation andinteractive user experience, while integrating real-time data from Gemini API to provide dynamic content. ",
-    technologies: ["CSS", "React"],
-    github: "https://github.com/Shivuu2803/Gemini-2.0",
-    hostedLink: "https://gemini-20-shivansh-mishra-s-projects.vercel.app/"
-  },
-  {
-    title: "Currency Convertor",
-    image: project11,
-    description:
-      "Currency Converter is a web tool for real-time currency conversion, featuring a simple interface and instant results. It highlights skills in API integration and practical financial application design.",
-    technologies: ["HTML", "CSS", "Javascript"],
-    github: "https://github.com/Shivuu2803/Currency-Converter-",
-    hostedLink: "https://shivuu2803.github.io/Currency-Converter-/"
+    hostedLink: "https://productapi-production-af22.up.railway.app/",
   },
   {
     title: "Dice Game",
@@ -131,15 +55,42 @@ export const PROJECTS = [
       "Dice Game is a web-based, two-player game where players roll dice to reach a target score. It features alternating turns, score tracking, and a risk element where rolling double six resets the player's score to zero, highlighting essential skills in game logic and interactive web development.",
     technologies: ["CSS", "React"],
     github: "https://github.com/Shivuu2803/Dice-Game",
-    hostedLink: "https://dice-game-pi-three.vercel.app/"
+    hostedLink: "https://dice-game-pi-three.vercel.app/",
+  },
+
+  {
+    title: "Gemini 2.0",
+    image: project6,
+    description:
+      "Developed Gemini 2.0, a React-based clone of the Gemini protocol with a redesigned user interface, integrating Gemini API for enhanced functionality. Leveraged React components, state management, and responsive design to ensure seamless navigation and interactive user experience, while integrating real-time data from Gemini API to provide dynamic content.",
+    technologies: ["CSS", "React"],
+    github: "https://github.com/Shivuu2803/Gemini-2.0",
+    hostedLink: "https://gemini-20-shivansh-mishra-s-projects.vercel.app/",
+  },
+  {
+    title: "Myfit App",
+    image: project9,
+    description:
+      "Developed myFit-App using React Native, focusing on cross-platform fitness tracking, intuitive user interfaces, seamless navigation, and best practices in state management and API integration.",
+    technologies: ["TailwindCSS", "React-Native"],
+    github: "https://github.com/Shivuu2803/myFit-App",
+  },
+  {
+    title: "Currency Converter",
+    image: project11,
+    description:
+      "Currency Converter is a web tool for real-time currency conversion, featuring a simple interface and instant results. It highlights skills in API integration and practical financial application design.",
+    technologies: ["HTML", "CSS", "Javascript"],
+    github: "https://github.com/Shivuu2803/Currency-Converter-",
+    hostedLink: "https://shivuu2803.github.io/Currency-Converter-/",
   },
   {
     title: "Simon Game",
     image: project8,
     description:
-      " fun Simon Game  incorporating HTML audio for engaging user experience,implementing sound sequences, and designing an intuitive interface to deliver an entertaining web app with enhanced gameplayand sound. ",
+      "A fun Simon Game incorporating HTML audio for an engaging user experience, implementing sound sequences, and designing an intuitive interface to deliver an entertaining web app with enhanced gameplay and sound.",
     technologies: ["HTML", "CSS", "Javascript", "jQuery"],
-    github: "https://github.com/Shivuu2803/Simon-Game-FrontEnd-"
+    github: "https://github.com/Shivuu2803/Simon-Game-FrontEnd-",
   },
   {
     title: "Drum Kit",
@@ -147,23 +98,17 @@ export const PROJECTS = [
     description:
       "Drum Kit is an interactive web-based project that allows users to play different drum sounds by pressing corresponding keys on the keyboard or clicking on-screen buttons. The project showcases the integration of sound effects with user interactions, creating a fun and engaging experience for practicing event handling and multimedia in web development.",
     technologies: ["HTML", "CSS", "Javascript"],
-    github: "https://github.com/Shivuu2803/Drum-Kit-FrontEnd-"
+    github: "https://github.com/Shivuu2803/Drum-Kit-FrontEnd-",
   },
-
   {
     title: "Empower-U",
     image: project10,
     description:
-      "EmpowerU is a digital platform for personal and professional growth, offering tailored learning experiences. It uses agile methods and user feedback to create a user-focused solution, helping individuals achieve their goals and enhance their well-being. ",
+      "EmpowerU is a digital platform for personal and professional growth, offering tailored learning experiences. It uses agile methods and user feedback to create a user-focused solution, helping individuals achieve their goals and enhance their well-being.",
     technologies: ["Canva", "Figma", "Product Development", "UI/UX Designing"],
-<<<<<<< HEAD
-    canva: "https://www.canva.com/design/DAF7RtHdrzA/Ljk31IqjAGABgAsrGXBoow/edit?utm_content=DAF7RtHdrzA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-=======
-    canva:"https://www.canva.com/design/DAF7RtHdrzA/Ljk31IqjAGABgAsrGXBoow/edit?utm_content=DAF7RtHdrzA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
->>>>>>> 5efdee5b13296de5591edbda92cb7ac8ea419fd1
+    canva:
+      "https://www.canva.com/design/DAF7RtHdrzA/Ljk31IqjAGABgAsrGXBoow/edit?utm_content=DAF7RtHdrzA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
   },
-
-
 ];
 
 export const CONTACT = {
